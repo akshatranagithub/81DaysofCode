3bdc7f8c40858fa1be21954db837230dad051f47
Akshat511716 <akshat511716@gmail.com>
2023-01-01 16:15:35 +0530

    Created a program for band name generator using string manipulation

---
+++ Day1/band-name-generator-end.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+first_name = input("What's the name of the city you grew up in? ")
+last_name = input("What's your pet's name? ")
+print(f"Your band name could be {first_name} {last_name}")
