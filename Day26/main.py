ec0af7ff05691217c0f5f3169fa7b3f2063875af
Akshat511716 <akshat511716@gmail.com>
2023-01-30 08:57:18 +0530

    Updated the python code with exception handling

--- Day26/main.py
+++ Day26/main.py
@@ -3,6 +3,16 @@ import pandas as pd
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-name = input("Enter a word: ").upper()
-output_list = [data_dict[letter] for letter in name if letter.isalpha()]
-print(output_list)
+
+def generate_phonetic():
+    name = input("Enter a word: ").upper()
+    try:
+        output_list = [data_dict[letter] for letter in name if letter.isalpha()]
+    except KeyError:
+        print("Please use letters only in the word")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
