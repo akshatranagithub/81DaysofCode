cf0269afd0c43c98679abae75264b6b8a69bcd22
Akshat Rana <akshat511716@gmail.com>
2023-02-23 09:53:33 +0530

    Created a python program to create basic flask app

---
+++ Day54/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p><strong>Hello, World!<strong></p>"
+
+
+if __name__ == '__main__':
+    app.run()
